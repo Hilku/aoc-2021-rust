95a25e411d35afe827f99ce6c26ff9c56027eee2
mkiss-crey <mkiss004@gmail.com>
2021-12-02 17:43:53 +0100

    day1 cleanup

--- day01/target/debug/day01.d
+++ day01/target/debug/day01.d
@@ -1,1 +1,1 @@
-C:\DEV\aoc-2021-rust\day01\target\debug\day01.exe: C:\DEV\aoc-2021-rust\day01\src\main.rs
+C:\DEV\aoc-2021-rust\day01\target\debug\day01.exe: C:\DEV\aoc-2021-rust\day01\src\day1input.txt C:\DEV\aoc-2021-rust\day01\src\main.rs
--- day01/target/debug/deps/day01.d
+++ day01/target/debug/deps/day01.d
@@ -1,5 +1,6 @@
-C:\DEV\aoc-2021-rust\day01\target\debug\deps\day01.exe: src\main.rs
+C:\DEV\aoc-2021-rust\day01\target\debug\deps\day01.exe: src\main.rs src\day1input.txt
 
-C:\DEV\aoc-2021-rust\day01\target\debug\deps\day01.d: src\main.rs
+C:\DEV\aoc-2021-rust\day01\target\debug\deps\day01.d: src\main.rs src\day1input.txt
 
 src\main.rs:
+src\day1input.txt:
