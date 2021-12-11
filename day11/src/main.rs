a3cd91bf3b9b35682da5d6034af3b261c6586334
Hilku <mkiss004@gmail.com>
2021-12-11 13:00:36 +0100

    Update main.rs

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,25 +1,19 @@
 fn main() {
     let input = include_str!("day11input.txt");
     let mut parts = input.lines();
-
-    let mut octopuses: Vec<Vec<i16>> = vec![vec![-1; 10]; 10];
-
+    let mut octopuses: Vec<Vec<i16>> = vec![Vec::new(); 10];
     let mut current_read_index = 0;
 
     while let Some(line) = parts.next() {
-        let mut second_index = 0;
         for c in line.chars() {
-            octopuses[current_read_index][second_index] = c.to_digit(10).unwrap() as i16;
-            second_index += 1;
+            octopuses[current_read_index].push(c.to_digit(10).unwrap() as i16)
         }
         current_read_index += 1;
     }
 
     let mut octopouses_clone = octopuses.clone();
     let mut flashes: usize = 0;
-
     let mut day: usize = 0;
-
     let mut flashed_octopy: Vec<(usize, usize)> = Vec::new();
     while flashed_octopy.len() < 100 {
         flashed_octopy.clear();
@@ -38,40 +32,23 @@ fn main() {
                         flashed_octopy.push((row, column));
                         flashed = true;
                         flashes += 1;
-                        if row > 0 {
-                            if !flashed_octopy.contains(&(row - 1, column)) {
-                                octopouses_clone[row - 1][column] += 1;
-                            }
-                            if column > 0 && !flashed_octopy.contains(&(row - 1, column - 1)) {
-                                octopouses_clone[row - 1][column - 1] += 1;
-                            }
-                            if column < octopouses_clone[row].len() - 1
-                                && !flashed_octopy.contains(&(row - 1, column + 1))
-                            {
-                                octopouses_clone[row - 1][column + 1] += 1;
+                        for i in -1..=1 {
+                            for q in -1..=1 {
+                                let row_index = row as i32 + i;
+                                let column_index = column as i32 + q;
+
+                                if !flashed_octopy
+                                    .contains(&(row_index as usize, column_index as usize))
+                                {
+                                    if let Some(v) = octopouses_clone.get_mut(row_index as usize) {
+                                        if let Some(j) = v.get_mut(column_index as usize) {
+                                            *j += 1;
+                                        }
+                                    }
+                                }
                             }
                         }
-                        if row < octopouses_clone.len() - 1 {
-                            if !flashed_octopy.contains(&(row + 1, column)) {
-                                octopouses_clone[row + 1][column] += 1;
-                            }
-                            if column < octopouses_clone[row].len() - 1
-                                && !flashed_octopy.contains(&(row + 1, column + 1))
-                            {
-                                octopouses_clone[row + 1][column + 1] += 1;
-                            }
-                            if column > 0 && !flashed_octopy.contains(&(row + 1, column - 1)) {
-                                octopouses_clone[row + 1][column - 1] += 1
-                            };
-                        }
-                        if column < octopouses_clone[row].len() - 1
-                            && !flashed_octopy.contains(&(row, column + 1))
-                        {
-                            octopouses_clone[row][column + 1] += 1;
-                        }
-                        if column > 0 && !flashed_octopy.contains(&(row, column - 1)) {
-                            octopouses_clone[row][column - 1] += 1;
-                        }
+
                         octopouses_clone[row][column] = 0;
                     }
                 }
