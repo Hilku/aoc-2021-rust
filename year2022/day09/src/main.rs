d2ea646286310d0056483fd84dc371520d805c20
Hilku <mkiss004@gmail.com>
2022-12-09 13:38:24 +0100

    day09

--- year2022/day09/src/main.rs
+++ year2022/day09/src/main.rs
@@ -112,22 +112,16 @@ fn main() {
             }
 
             for i in 1..tail_coordinates.len() {
-                let cor: Coordinate = move_tail(
-                    tail_coordinates[i - 1].clone(),
-                    tail_coordinates[i].clone(),
-                    dir.to_string(),
-                );
+                let cor: Coordinate =
+                    move_tail(tail_coordinates[i - 1].clone(), tail_coordinates[i].clone());
                 tail_coordinates[i] = cor.clone();
                 if i == tail_coordinates.len() - 1 {
                     if !part2_positions.contains(&tail_coordinates[i]) {
-                        println!("new position for endtail: {:?}", tail_coordinates[i]);
                         part2_positions.push(tail_coordinates[i].clone());
                     }
                 }
             }
         }
-
-        println!("command: {}, tail pos: {:?}", line, tail_coordinates[6]);
     }
 
     println!("Coordinates for tail1: {}", tail_positions.len());
@@ -135,53 +129,39 @@ fn main() {
     println!("Coordinates for tail9: {}", part2_positions.len());
 }
 
-fn move_tail(tail_before: Coordinate, tail_to_move: Coordinate, direction: String) -> Coordinate {
-    let mut end_result = tail_to_move.clone();
-    if direction == "R" {
-        if tail_before.x - tail_to_move.x > 1 {
-            end_result.x += 1;
-
-            if tail_to_move.y < tail_before.y {
-                end_result.y += 1;
-            } else if tail_to_move.y > tail_before.y {
-                end_result.y -= 1;
-            }
-        }
-    }
-
-    if direction == "L" {
-        if tail_to_move.x - tail_before.x > 1 {
-            end_result.x -= 1;
-
-            if tail_to_move.y < tail_before.y {
-                end_result.y += 1;
-            } else if tail_to_move.y > tail_before.y {
-                end_result.y -= 1;
-            }
-        }
-    }
-
-    if direction == "U" {
-        if tail_before.y - tail_to_move.y > 1 {
+fn move_tail(tail_before: Coordinate, tail_to_move: Coordinate) -> Coordinate {
+    let mut end_result: Coordinate = tail_to_move.clone();
+    if tail_before.x - tail_to_move.x > 1 {
+        end_result.x += 1;
+        if tail_before.y - tail_to_move.y > 0 {
             end_result.y += 1;
-
-            if tail_to_move.x < tail_before.x {
-                end_result.x += 1;
-            } else if tail_to_move.x > tail_before.x {
-                end_result.x -= 1;
-            }
         }
-    }
-
-    if direction == "D" {
-        if tail_to_move.y - tail_before.y > 1 {
+        if tail_before.y - tail_to_move.y < 0 {
             end_result.y -= 1;
-
-            if tail_to_move.x < tail_before.x {
-                end_result.x += 1;
-            } else if tail_to_move.x > tail_before.x {
-                end_result.x -= 1;
-            }
+        }
+    } else if tail_before.x - tail_to_move.x < -1 {
+        end_result.x -= 1;
+        if tail_before.y - tail_to_move.y > 0 {
+            end_result.y += 1;
+        }
+        if tail_before.y - tail_to_move.y < 0 {
+            end_result.y -= 1;
+        }
+    } else if tail_before.y - tail_to_move.y > 1 {
+        end_result.y += 1;
+        if tail_before.x - tail_to_move.x > 0 {
+            end_result.x += 1;
+        }
+        if tail_before.x - tail_to_move.x < 0 {
+            end_result.x -= 1;
+        }
+    } else if tail_before.y - tail_to_move.y < -1 {
+        end_result.y -= 1;
+        if tail_before.x - tail_to_move.x > 0 {
+            end_result.x += 1;
+        }
+        if tail_before.x - tail_to_move.x < 0 {
+            end_result.x -= 1;
         }
     }
 
