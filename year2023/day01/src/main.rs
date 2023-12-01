b89cf1fb20ba68a2dd52be2d913f47980a40f026
Hilku <mkiss004@gmail.com>
2023-12-01 06:24:05 +0100

    2023 day 1

--- year2023/day01/src/main.rs
+++ year2023/day01/src/main.rs
@@ -9,35 +9,41 @@ fn main() {
     let mut current_elf_calories : i32 = 0;
 
     while let Some(string) = split_stuff.next() {
-        let line = string.parse::<i32>();
-        if let Ok(asd) = line {
-            current_elf_calories += asd;
+        let mut replaced_string = string.replace("one", "o1ne");    
+        replaced_string = replaced_string.replace("two", "t2wo");
+        replaced_string = replaced_string.replace("three", "thr3e");
+        replaced_string = replaced_string.replace("four", "f4ur");
+        replaced_string = replaced_string.replace("five", "f5ve");
+        replaced_string = replaced_string.replace("six", "s6x");
+        replaced_string = replaced_string.replace("seven", "s7ven");
+        replaced_string = replaced_string.replace("eight", "e8ight");
+        replaced_string = replaced_string.replace("nine", "n9ne");
+
+        let mut first_digit = true;
+        let mut number_str = String::new();    
+        let mut last_digit_char = '0';
+        for c in replaced_string.chars() {
+                if c.is_digit(10)
+                {
+                    if first_digit
+                    {
+                        first_digit = false;
+                        number_str.push(c);
+                    }
+                    last_digit_char = c;
+                }
         }
-        else
-        {
-            parsed_vec.push(current_elf_calories);
-            current_elf_calories = 0;
-        }
+        number_str.push(last_digit_char);
+        parsed_vec.push(number_str.parse::<i32>().unwrap());
     }
 
-    let mut highest_number : i32 = 0;
-
-    //part1
-
+    let mut sum = 0;
     for number in parsed_vec.iter()
     {
-        if *number > highest_number
-        {
-            highest_number = *number;
-        }
+        println!("{}", number);
+        sum += number;
     }
 
-    println!("Highest number:{}", highest_number);
-
-    //part2
-
-    parsed_vec.sort();
-
-    println!("highest 3: {}", parsed_vec.iter().rev().take(3).sum::<i32>());
+    println!("sum:{}", sum);
 
 }
